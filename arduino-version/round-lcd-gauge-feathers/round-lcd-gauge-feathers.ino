9d6c2a0c48b41b1c3aa17f3a8cfdbcac66d01b21
Glen Akins <ktmglen@glenakins.com>
2021-01-05 20:28:35 -0700

    cut rotate code in half; still need to cut alpha blend code in half, still need to compute pointer bounding box and trim rotate to bounding box

--- arduino-version/round-lcd-gauge-feathers/round-lcd-gauge-feathers.ino
+++ arduino-version/round-lcd-gauge-feathers/round-lcd-gauge-feathers.ino
@@ -39,12 +39,14 @@ void DisplayGauge (const uint32_t *dial, const uint32_t *needle, float angle)
 
   int32_t  i, j, xcen, ycen, wm2, hm2;
   int32_t  xdif, ydif, xpm, ypm, xp, yp, xf, yf;
-  int32_t  rval, gval, bval, aval;
+  int32_t rbval, gaval;
   uint32_t word00, word01, word10, word11;
   const uint32_t *lines;
   float  sina, cosa;
-
+  const uint32_t mask = 0x00ff00ff;
+  
   const uint32_t *ptrs1;
+  uint32_t rgba_background;
   uint8_t lcdbuffer[2*240];
   uint8_t *lcdbuffp;
 
@@ -84,10 +86,8 @@ void DisplayGauge (const uint32_t *dial, const uint32_t *needle, float angle)
 
       if (xp < 0 || yp < 0 || xp > wm2 || yp > hm2) {
         // if off the edge, write input colorval
-        rval = 0;
-        gval = 0;
-        bval = 0;
-        aval = 0;
+        rbval = 0;
+        gaval = 0;
       } else {
         // do area weighting.  Without this, we would
         // simply do:
@@ -100,30 +100,31 @@ void DisplayGauge (const uint32_t *dial, const uint32_t *needle, float angle)
         word10 = *(lines + xp + 1);
         word01 = *(lines + 240 + xp);
         word11 = *(lines + 240 + xp + 1);
-        
-        rval = ((16 - xf) * (16 - yf) * ((word00 >> 24) & 0xff) +
-                xf * (16 - yf) * ((word10 >> 24) & 0xff) +
-                (16 - xf) * yf * ((word01 >> 24) & 0xff) +
-                xf * yf * ((word11 >> 24) & 0xff) + 128) / 256;
-        gval = ((16 - xf) * (16 - yf) * ((word00 >> 16) & 0xff) +
-                xf * (16 - yf) * ((word10 >> 16) & 0xff) +
-                (16 - xf) * yf * ((word01 >> 16) & 0xff) +
-                xf * yf * ((word11 >> 16) & 0xff) + 128) / 256;
-        bval = ((16 - xf) * (16 - yf) * ((word00 >> 8) & 0xff) +
-                xf * (16 - yf) * ((word10 >> 8) & 0xff) +
-                (16 - xf) * yf * ((word01 >> 8) & 0xff) +
-                xf * yf * ((word11 >> 8) & 0xff) + 128) / 256;
-        aval = ((16 - xf) * (16 - yf) * ((word00 >> 0) & 0xff) +
-                xf * (16 - yf) * ((word10 >> 0) & 0xff) +
-                (16 - xf) * yf * ((word01 >> 0) & 0xff) +
-                xf * yf * ((word11 >> 0) & 0xff) + 128) / 256;
+
+        rbval =
+               ((16 - xf) * (16 - yf) * ((word00 >> 8) & mask) +
+                xf * (16 - yf) * ((word10 >> 8) & mask) +
+                (16 - xf) * yf * ((word01 >> 8) & mask) +
+                xf * yf * ((word11 >> 8) & mask) + 128 + (128<<16)) >> 8;
+
+        gaval =
+               ((16 - xf) * (16 - yf) * (word00 & mask) +
+                xf * (16 - yf) * (word10 & mask) +
+                (16 - xf) * yf * (word01 & mask) +
+                xf * yf * (word11 & mask) + 128 + (128<<16)) >> 8;
       }
       
+
       //----------------------------------------
       // blend
       //----------------------------------------
 
-      uint32_t rgba_background = *ptrs1++;
+      rgba_background = *ptrs1++;
+
+      uint8_t rval = rbval >> 16;
+      uint8_t gval = gaval >> 16;
+      uint8_t bval = rbval;
+      uint8_t aval = gaval;
 
       uint8_t rbg = rgba_background >> 24;
       uint8_t gbg = rgba_background >> 16;
